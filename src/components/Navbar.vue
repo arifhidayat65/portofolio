7ac95d82a41025d4cea0fb8e04279279760f900d
arif <arifhidayat1010@gmail.com>
2023-03-21 15:41:28 +0700

    [UPDATE] change style redesign about

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,8 +31,10 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mx-2">
+          <ul class="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm" id="pillNav2" role="tablist" style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active rounded-5" id="home-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="true">About</button>
+            
               <a
                 class="nav-link"
                 href="/about"
