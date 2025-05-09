33e79bd3b0698ff15db3afc1fb682af95251d0c2
Arif Hidayat <arifhidayat1010@gmail.com>
2025-05-10 06:26:23 +0700

    add config.jsads

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ let config = {
   }
 };
 
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8616854468199151"
+     crossorigin="anonymous"></script>
 export default config;
